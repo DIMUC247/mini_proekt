68e3a5f523a37186cadd7577674340a7e41960ba
DIMUC247 <dimuch.v09@gmail.com>
2025-04-15 20:45:07 +0200

    1st commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from requests_html import HTMLSession,AsyncHTMLSession
 from fastapi import FastAPI, Path,Query,HTTPException,status
 from fastapi.responses import JSONResponse
 import re
+import uvicorn
 
 
 app = FastAPI()
@@ -12,7 +13,7 @@ app = FastAPI()
 async def fetch_url_with_aiohttp(url:str) ->str:
     async with ClientSession() as session:
         response = await session.get(url)
-        if response.status != 200:
+        if response.status == 200:
             return await response.text(encoding="utf-8")
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
 
@@ -24,8 +25,8 @@ async def fetch_url_with_requests_html(url:str) -> str:
         return response.html
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
 
-url = "https://uk.wikipedia.org/wiki/%D0%92%D0%BE%D0%BB%D1%82_%D0%94%D1%96%D1%81%D0%BD%D0%B5%D0%B9"
-tag = "b"
+url = "https://uk.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%BE-%D1%83%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%81%D1%8C%D0%BA%D0%B0_%D0%B2%D1%96%D0%B9%D0%BD%D0%B0_(%D0%B7_2014)"
+tag = "p"
 found_text = "До цього числа входять"
 
 # ------------------------------------------------------
@@ -58,14 +59,16 @@ else:
 # ------------------------------------------------------
 
 @app.get("/data/{tag}")
-async def get_data(text: str = Query(...),url: str = Query(...)):
+async def get_data(text: str = Query(...),url: str = Query(...),tag = Path(...)):
     html = await fetch_url_with_aiohttp(url)
     soup = BeautifulSoup(html, "lxml")
     text = soup.find(string=re.compile(text)).find_parent(tag)
     if text:
-        return JSONResponse(content={"text": text.text})
+        return JSONResponse(content={"text": text.get_text()})
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Тексту не знайдено")
 
 
+if __name__ == "__main__":
+    uvicorn.run(app)
 
